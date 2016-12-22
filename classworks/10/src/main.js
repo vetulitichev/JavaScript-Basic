688a0165530df281a08e950620cc70c11059faf1
Oleg <oleglustenko@gmail.com>
2016-12-23 00:36:48 +0200

    homework 9 update last task

---
+++ classworks/10/src/main.js
@@ -0,0 +1,26 @@
+const store = {};
+store.sell = function(book) {};
+
+store.add = function(goods) {};
+
+store.giveSalary = function() {};
+
+store.employees = [
+  { name: "Vera", salary: 500 },
+  { name: "Jack", salary: 200 },
+  { name: "John", salary: 2500 }
+];
+
+store.books = [
+  { name: 'Harry Potter', pages: 510, price: '15$' },
+  { name: 'Eloquent JavaScript', pages: 300, price: '20$' },
+  { name: 'Secrets of JavaScript ninjas', pages: 500, price: '35$' },
+  { name: 'JavaScript Patterns', pages: 200, price: '3.25$' },
+];
+
+store.stock = [
+  { name: 'Happy New Year', pages: 1, price: '1$' },
+  { name: 'Learning Windows in 3 steps', pages: 800, price: '100$' },
+  { name: 'Happy birthday', pages: 1, price: '30$' },
+  { name: 'Box', id: 1, items: ['pens', 'pencils', 'sticks'], price: '1$' }
+];
--- classworks/9/home/home.js
+++ classworks/9/home/home.js
@@ -90,19 +90,20 @@ myObject.myFilter('name');
 
 let junior = {};
 
-// fn.length ==
+// fn.length == arguments.length
 
 function addMethod(object, name, fn) {
   
 }
 
 
-addMethod(junior, 'ok', function() {console.log(arguments.length)});
-addMethod(junior, 'ok', function(one) {console.log(arguments.length)});
-addMethod(junior, 'ok', function(one, two) {console.log(arguments.length)});
-addMethod(junior, 'ok', function(one, two, three) {console.log(arguments.length)});
+addMethod(junior, 'ok', function() {console.log('zero arguments')});
+addMethod(junior, 'ok', function(one) {console.log('one arguments')});
+addMethod(junior, 'ok', function(one, two) {console.log('two arguments')});
+addMethod(junior, 'ok', function(one, two, three) {console.log('three arguments')});
 
-junior.ok(1);
-junior.ok(1, 2);
-junior.ok(1, 2, 3);
+junior.ok(); //'zero arguments'
+junior.ok(1); //'one arguments'
+junior.ok(1, 2); // 'two arguments'
+junior.ok(1, 2, 3); // 'three arguments'
 
