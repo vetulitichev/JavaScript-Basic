91b61c85d3f07ab1a1e3f67891f1fdaafe8fa9e8
Oleg <oleglustenko@gmail.com>
2017-01-21 15:07:48 +0200

    LEsson 15

--- classworks/15/src/main.js
+++ classworks/15/src/main.js
@@ -6,26 +6,37 @@ const app = {
     { name: 'Вопрос 2', answers: ['Ответ 1', 'Ответ 2', 'Ответ 3'] }, // прилетает с серверва
     { name: 'Вопрос 3', answers: ['Ответ 1', 'Ответ 2', 'Ответ 3'] } // прилетает с серверва
   ],
-
-  questionHeader(name) {
+  questionHeader(question) {
     const li = this.new('li');
     const h2 = this.new('h2');
     const ul = this.new('ul');
-    h2.textContent = name;
+    h2.textContent = question.name;
+
+    if (question.id == 1) {
+      h2.id = 'newId'
+    }
+
     li.append(h2, ul);
     this.ol.append(li);
     return ul;
   },
   render() {
-    const body = document.body;
+    const table = document.querySelector('.table');
     const main = this.new('main');
     const header = this.new('h1');
+    header.id = 'privet';
+    const button = this.new('button');
+    button.textContent = 'Some button';
 
     header.textContent = this.testName;
 
-    this.questions.forEach(question => {
+    this.questions.forEach((question, index) => {
+      console.log(index);
       const name = question.name;
-      const ul = this.questionHeader(name);
+      if (index == 1) {
+        question.id = index;
+      }
+      const ul = this.questionHeader(question);
 
       question.answers.forEach(answer => {
         const li = this.new('li');
@@ -42,9 +53,8 @@ const app = {
 
     main.append(header);
     main.append(this.ol);
+    main.append(button);
     body.append(main);
-
-
   },
   new(elemName) {
     return document.createElement(elemName);
@@ -52,3 +62,21 @@ const app = {
 }
 
 app.render();
+
+
+
+const allNodes = document.querySelectorAll('*');
+
+// for(let i =0;i<allNodes.length; i++) {
+//   const elem = allNodes[i];
+//   if(elem.textContent) {
+//     console.log(elem.textContent);
+//   }
+// }
+
+for (let i = 0; i < allNodes.length; i++) {
+  const elem = allNodes[i];
+  if (elem.tagName == 'H2') {
+    console.log(elem.textContent);
+  }
+}
