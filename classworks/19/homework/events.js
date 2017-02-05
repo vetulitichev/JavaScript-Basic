292a192f68fafc7806292ac6ece6b79018c1469e
Oleg <oleglustenko@gmail.com>
2017-02-05 11:21:50 +0200

    ok

--- classworks/19/homework/events.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-  [...document.querySelectorAll('section>div')]
-    .map(block => {
-      return { block, width: block.offsetWidth };
-    })
-    .forEach(({ block, width }) => {
-      block.style.flexBasis = width + 'px';
-    });
-};
